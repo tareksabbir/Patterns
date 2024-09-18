74ac0b26294253a81b76fb695053f4cce5470e61
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-19 03:41:55 +0600

    component setup

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+            
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './Root/Root';
+import Home from './Home/Home';
 
 const router = createBrowserRouter([
   {
@@ -14,7 +15,7 @@ const router = createBrowserRouter([
     children:[
       {
         path:"/",
-        element:<div></div>
+        element:<Home></Home>
       }
     ]
   },
