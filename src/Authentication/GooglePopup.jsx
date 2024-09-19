23b05548091a3677ef45a54ddd751fddb63d6f14
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-20 02:07:36 +0600

    google pop up practice

--- src/Authentication/GooglePopup.jsx
+++ src/Authentication/GooglePopup.jsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import app from "@/Firebase/Firebase.init";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 
 const GooglePopup = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider()
+    const [googleUser,setGoogleUser] = useState([])
+    const googlePopUp =()=>{
+        signInWithPopup(auth, provider)
+        .then(result =>{
+            const user = result.user
+            setGoogleUser(user)
+        })
+        .then(error =>{
+            console.log(error)
+        })
+    }
+
     return (
+        <>
         <div className="flex items-center justify-center">
-        <Button>Click me</Button>
+        <Button onClick={googlePopUp}>Click me</Button>
+        
         </div>
+        <h1 className="mt-5 text-center">{googleUser?.email}</h1>
+        </>
     );
 };
 
