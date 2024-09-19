0708d41ddf6d8515b8380c3a5274022cb4cf0e28
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-20 03:21:45 +0600

    pop up update

--- src/Authentication/GooglePopup.jsx
+++ src/Authentication/GooglePopup.jsx
@@ -1,33 +1,53 @@
 import { Button } from "@/components/ui/button";
 import app from "@/Firebase/Firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { useState } from "react";
 
-
 const GooglePopup = () => {
-    const auth = getAuth(app);
-    const provider = new GoogleAuthProvider()
-    const [googleUser,setGoogleUser] = useState([])
-    const googlePopUp =()=>{
-        signInWithPopup(auth, provider)
-        .then(result =>{
-            const user = result.user
-            setGoogleUser(user)
-        })
-        .then(error =>{
-            console.log(error)
-        })
-    }
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const [googleUser, setGoogleUser] = useState(null);
+  const googlePopUp = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const LoggedInUser = result.user;
+        setGoogleUser(LoggedInUser);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <>
-        <div className="flex items-center justify-center">
-        <Button onClick={googlePopUp}>Click me</Button>
-        
-        </div>
-        <h1 className="mt-5 text-center">{googleUser?.email}</h1>
-        </>
-    );
+  const handleSignOutGoogle = () => {
+    signOut(auth)
+      .then((result) => {
+        // Sign-out successful.
+        console.log(" Sign-out successful.", result);
+        setGoogleUser(null);
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+  console.log(googleUser);
+  return (
+    <>
+      <div className="flex items-center justify-center gap-5">
+        {googleUser ? (
+          <button onClick={handleSignOutGoogle}>Sign Out</button>
+        ) : (
+          <Button onClick={googlePopUp}>Sign In</Button>
+        )}
+      </div>
+      {googleUser && <h1 className="mt-5 text-center ">{googleUser.email}</h1>}
+    </>
+  );
 };
 
 export default GooglePopup;
