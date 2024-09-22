ac277910ef155f375d24aa01100474cf086a9e1b
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-09-22 12:06:26 +0600

    update

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -2,24 +2,17 @@ import cover from "../assets/cover.png";
 
 const Banner = () => {
   return (
-    <div>
+    <div >
       <div className="container mx-auto md:mt-8 ">
         <h1 className="font-light md:text-5xl sm:text-2xl">
           We are on the same side
         </h1>
         <h1 className="md:text-[12.5rem]  sm:text-[5rem] font-bold lg:mt-[-3rem]">
-          Design is our
+         Design is our
         </h1>
       </div>
-      {/* <div className="relative w-full">
-        <img src={cover} alt="" className="object-cover w-full " />
-        <div className="container mx-auto">
-          <h1 className="md:text-[15.5rem]  sm:text-[5rem] font-bold ">
-            Passion
-          </h1>
-        </div>
-      </div> */}
-      <div className="relative w-full h-[50vh] overflow-hidden lg:mt-[-2rem]">
+      
+      <div className="relative w-full lg:h-[50vh] overflow-hidden lg:mt-[-2rem]">
         <img src={cover} alt="" className="object-cover w-full h-full" />
         <div className="container absolute top-0 left-0 right-0 mx-auto md:mt-[-4rem]">
           <h1 className="md:text-[12.5rem] sm:text-[5rem] font-bold text-white text-shadow ">
@@ -28,7 +21,7 @@ const Banner = () => {
         </div>
       </div>
     </div>
-    
+     
   );
 };
 
